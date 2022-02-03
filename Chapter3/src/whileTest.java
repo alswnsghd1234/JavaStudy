5be963cda1165844170fffbed827eb2510761d5e
JunHong <alswnsghd1234@naver.com>
2022-02-03 18:31:12 +0900

    d

--- Chapter3/src/whileTest.java
+++ Chapter3/src/whileTest.java
@@ -1,4 +1,26 @@
 
+import java.util.Scanner;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
 public class whileTest {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int input;
+		int sum = 0;
+		
+		input = scanner.nextInt();
+		
+		do {
+			input = scanner.nextInt();
+			sum += input;
+
+		} while( input != 0);
+		System.out.println(sum);
+	}
+
+
 
 }
---
+++ ch02/src/ch02/PrintEx1.java
@@ -0,0 +1,8 @@
+package ch02;
+
+public class PrintEx1 {
+
+	public static void main(String[] args) {
+		System.out.printf("%d", 0x1A);
+	}
+}
