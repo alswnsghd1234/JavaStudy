29aaefd3a5e6b59890580fc3e26611691e84830b
JunHong <alswnsghd1234@naver.com>
2022-02-05 00:55:52 +0900

    update

--- ch03/src/Ex3_11.java
+++ ch03/src/Ex3_11.java
@@ -1,7 +1,20 @@
 class Ex3_11 {
 	public static void main(String args[]) { 
-		double pi = 3.141592; 
-		double shortPi = Math.round(pi * 1000) / 1000.0;
-		System.out.println(shortPi);
+//		double pi = 3.131592;
+//		
+//		System.out.println(pi);
+//		System.out.println(pi*1000);
+//		System.out.println(Math.round(pi*1000));
+//		System.out.println(Math.round(pi*1000)/1000); //3
+//		System.out.println((double)Math.round(pi*1000)/1000); // 3.142
+		
+		double pi = 3.131592;
+		
+//		pi = pi * 1000;
+//		System.out.println((int)pi/1000.0);
+		
+		System.out.println(pi*1000);
+		System.out.println((int)(pi*1000));
+		System.out.println((int)(pi*1000)/1000.0);
 	}
 }
